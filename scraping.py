10a013540e6461c307d0014c072199e7fad2f8a6
Isaac Kapeel <ikapeel@gmail.com>
2025-08-01 05:04:38 -0500

    Cleaned up code and removed useless comments

--- scraping.py
+++ scraping.py
@@ -5,7 +5,6 @@ import requests
 def filmTitle(data):
     title_name = data.find("a")
     if title_name:
-        #print(title_name.string)
         return title_name.string
 
 def filmImage(data):
@@ -15,30 +14,22 @@ def filmImage(data):
 def filmRelease(data):
     release_year = data.find("span")
     if release_year:
-        #print(release_year.string)
         return release_year.string
 
 def filmRating(data):
     # user_rating_star = data.find("span")
     user_rating_number = data.find("input")
-    # if user_rating_star.string != "":
-    #     print(user_rating_star.string)
-    #     if user_rating_number:
-    #         print(user_rating_number["value"])
     if user_rating_number:
         return user_rating_number["value"]
     else:
-        #print("User didn't rate the film")
         return False
 
 def filmReview(data):
     if "icon-status-off" in data.get("class", []):
-        # print("User has no review")
         return False
     else:
         partial_url = data.find("a")
         full_url = f"https://letterboxd.com{partial_url["href"]}"
-        #print(f"User's film review: {full_url}")
         return full_url
     
 
@@ -79,7 +70,6 @@ def firstScrape(profile):
         if not user or "not found" in user.text.lower():
             print(f"Invalid or non-existent user: {profile}")
             return False
-        #print((user.string.split("’")[0]).strip())
         tbody = doc.tbody
         trs = tbody.find_all("tr")
         date, details, released, rating, like, rewatch, review = trs[0].find_all("td")[1:8]
@@ -92,10 +82,3 @@ def firstScrape(profile):
     except Exception as e:
         print(f"Error retrieving {profile} info: ", e)
         return False
-
-def main():
-    pass
-    
-
-if __name__ == "__main__":
-    main()
